18a11a309972f9c4892543bfcb00417a7df92d5a
Enchen San <48435089+srcenchen@users.noreply.github.com>
2023-08-28 16:00:37 +0800

    :construction_worker: CI 构建系统 (#1)
    * :construction_worker: ci support
    
    * :construction_worker: update CI build system
    
    * :construction_worker: Docker支持 CI 打包优化

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"eGZ-GZTV/internal/cmd"
 	_ "eGZ-GZTV/internal/packed"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gcfg"
 
 	"github.com/gogf/gf/v2/os/gctx"
-
-	"eGZ-GZTV/internal/cmd"
 )
 
 func main() {
+	// 设置默认配置文件目录
+	_ = g.Cfg().GetAdapter().(*gcfg.AdapterFile).AddPath("manifest/conf")
+	_ = g.Cfg().GetAdapter().(*gcfg.AdapterFile).AddPath("conf")
 	cmd.Main.Run(gctx.GetInitCtx())
 }
