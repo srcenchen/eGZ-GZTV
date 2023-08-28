48fabf3b9e5ba18a68f4b1205625dba7ad825fa3
srcenchen <srcenchen@gmail.com>
2023-08-28 21:11:18 +0800

    :ambulance: 修复登录功能 close #8

--- api/setting/v1/notice.go
+++ api/setting/v1/notice.go
@@ -15,11 +15,11 @@ type GetNoticeRes struct {
 
 // SetNoticeReq /** 设置公告
 type SetNoticeReq struct {
-	g.Meta `method:"POST" summary:"设置公告" tags:"设置"`
+	g.Meta `method:"GET" summary:"设置公告" tags:"设置"`
 	Notice string `v:"required#公告不能为空"` // 公告
 }
 
 // SetNoticeRes /** 设置公告
 type SetNoticeRes struct {
-	g.Meta `method:"POST" summary:"设置公告" tags:"设置"`
+	g.Meta `method:"GET" summary:"设置公告" tags:"设置"`
 }
